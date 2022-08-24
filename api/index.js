e882ad098befc0d53c684cda545f166b742108ce
Sukit Bunsiri <100616257+Neolization@users.noreply.github.com>
2022-08-25 03:27:44 +0700

    Update cors domain

--- api/index.js
+++ api/index.js
@@ -25,7 +25,7 @@ if (config.isVercel) {
 
   app.use(
     cors({
-      origin: 'https://offit.vercel.app/',
+      origin: 'https://offit.vercel.app',
       credentials: true,
       optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
     })
